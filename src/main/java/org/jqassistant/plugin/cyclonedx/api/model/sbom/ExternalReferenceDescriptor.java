eb735cd417871533412f7e66ec435e90ef5128c6
Dirk Mahler <dirk.mahler@buschmais.com>
2023-08-04 12:54:14 +0200

    upgraded to jQA plugin parent 2.0.5

--- src/main/java/org/jqassistant/plugin/cyclonedx/api/model/sbom/ExternalReferenceDescriptor.java
+++ src/main/java/org/jqassistant/plugin/cyclonedx/api/model/sbom/ExternalReferenceDescriptor.java
@@ -1,20 +1,14 @@
 package org.jqassistant.plugin.cyclonedx.api.model.sbom;
 
-import java.util.List;
-
 import com.buschmais.xo.neo4j.api.annotation.Label;
-import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 import org.jqassistant.plugin.cyclonedx.api.model.CycloneDXDescriptor;
 
 @Label("ExternalReference")
-public interface ExternalReferenceDescriptor extends CycloneDXDescriptor, UrlTemplate, TypeTemplate {
+public interface ExternalReferenceDescriptor extends CycloneDXDescriptor, UrlTemplate, TypeTemplate, HashesTemplate {
 
     String getComment();
 
     void setComment(String comment);
-
-    @Relation("HAS_HASH")
-    List<HashDescriptor> getHashes();
 
 }
